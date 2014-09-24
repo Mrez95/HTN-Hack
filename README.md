d2bb03fb5617c0da82d62e76fed9aec2fe607cf3
Mrez95 <ez95@live.ca>
2014-09-23 21:21:31 -0400

    Post hackathon touchups
    Polished and cleaned project for demo

---
+++ README.md
@@ -0,0 +1,19 @@
+HackTheNorth
+============
+
+Our Myo hack for the HackTheNorth Hackathon September 19th 2014
+
+##Requirements##
+In order to build, you first need to add these Standard Assets. Click on `Assets>Import Package>` for the following Assets:
+	
+- Character Control
+- Scripts
+- Skyboxes
+- Terrain
+
+Additionally, this project uses:
+- the Rune Sword asset located [here in the Unity Asset Store](https://www.assetstore.unity3d.com/en/#!/content/12152)
+- OculusUnityIntegration unitypackage and the Oculus SDK
+
+##TODO in the Future##
+- Keep gitignore updated in such a way that large asset files and samples are ignored
---
+++ unity/Assets/Scripts/SwordAttackScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordAttackScript : MonoBehaviour {
+
+	private bool inCollision = false;
+	public bool friendlyFire = false;
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("It hit something!");
+		string hitted = collision.collider.name;
+		//only detects if the collider is a character, else do nothing
+		bool isParent = gameObject.GetComponent<Transform> ().parent.name.Equals (hitted);
+		if (hitted.Contains("CharBody") && !inCollision && (!isParent || friendlyFire)) {
+			//we can integrate something that can calculate the damage from how hard the swing is here
+			CharacterProperties hitChar = GameObject.Find(hitted).GetComponent<CharacterProperties>();
+			float yAngle = gameObject.GetComponentInParent<Transform>().GetComponentInParent<Transform>().eulerAngles.y;
+			hitChar.HpLoss(20, yAngle);
+			inCollision = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collision){
+		Debug.Log ("The sword left the body");
+		string hitted = collision.collider.name;
+		//only detects if the collider is a character, else do nothing
+		if (inCollision) {
+			inCollision = false;
+		}
+	}
+}
